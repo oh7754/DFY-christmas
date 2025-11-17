0216ce2c91a2bc6a57ca25190b9cc75530c82d69
oh7754 <ojh7754@gmail.com>
2025-11-17 18:47:42 +0900

    복구중

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+console.log("🔥 main.js loaded - version");
 // ===== three.js & GLTFLoader (CDN ESM) =====
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/loaders/GLTFLoader.js";
